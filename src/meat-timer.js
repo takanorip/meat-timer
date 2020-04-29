72fe90a607008a513520ef6d151140aca323a8e2
takanori <takanori0170@gmail.com>
2020-04-29 15:22:05 +0900

    init

---
+++ src/meat-timer.js
@@ -0,0 +1,3 @@
+import { MeatTimer } from './MeatTimer.js';
+
+customElements.define('meat-timer', MeatTimer);
---
+++ stories/meat-timer.stories.js
@@ -0,0 +1,11 @@
+import { html } from 'lit-html';
+import '../src/meat-timer.js';
+
+export default {
+  title: 'meat-timer',
+};
+
+export const App = () =>
+  html`
+    <meat-timer></meat-timer>
+  `;
